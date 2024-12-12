8d79c3ffa9d8417aa84835f4fa4da7a3d2dffafb
PLINIORZAVALA <134644204+PLINIORZAVALA@users.noreply.github.com>
2024-12-11 20:33:22 -0500

    configuracion de base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { hostname } from 'os';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',  // Asegúrate de que el usuario y la contraseña sean correctos
+      password: '',      // Si tienes una contraseña, colócala aquí
+      database: 'IOCSA',  // Asegúrate de que esta base de datos exista
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Evitar en producción, en desarrollo puedes usar true si quieres que la base se sincronice automáticamente
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
